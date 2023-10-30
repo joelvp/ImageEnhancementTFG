4738ee11a28a912ef80226cd11c23fe09a79fb4f
joelvp <jvaldep@teleco.upv.es>
2023-10-30 15:27:22 +0100

    ADD lowLight initial structure

--- src/enhancement.py
+++ src/enhancement.py
@@ -7,6 +7,8 @@ def get_args():
     parser = argparse.ArgumentParser(description='Image Enhancement.')
     parser.add_argument('--WB', action='store_true',
                         help="Applying automatic WB")
+    parser.add_argument('--lowlight', action='store_true',
+                        help="Low-light enhancement")
     
     return parser.parse_args()
 
@@ -15,8 +17,11 @@ if __name__ == "__main__":
     args = get_args()
     
     wb = args.WB
+    llight = args.lowlight
     
     if wb:
+        white_balance()
+    if llight:
         white_balance()
     else:
         raise Exception('No enhancement applied!')
---
+++ src/models/LLFlow/code/lowlight.py
@@ -0,0 +1,42 @@
+from models.LLFlow.code.test_unpaired import *
+
+def lowlight(conf_path="./models/LLFlow/code/confs/LOLv2-pc.yml",out_dir="../data/demo_images/output/"):
+
+    conf = conf_path.split('/')[-1].replace('.yml', '')
+
+    model, opt = load_model(conf_path)
+    # model.netG = model.netG.to("cpu")
+    model.netG = model.netG.cuda()
+
+    lr_dir = opt['dataroot_unpaired']
+    lr_paths = fiFindByWildcard(os.path.join(lr_dir, '*.*'))
+
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    #test_dir = os.path.join(this_dir, '..', 'results', conf, args.name)
+    print(f"Out dir: {out_dir}")
+
+    for lr_path, idx_test in tqdm.tqdm(zip(lr_paths, range(len(lr_paths)))):
+
+        lr = imread(lr_path)
+        raw_shape = lr.shape
+        lr, padding_params = auto_padding(lr)
+        his = hiseq_color_cv2_img(lr)
+        if opt.get("histeq_as_input", False):
+            lr = his
+
+        lr_t = t(lr)
+        if opt["datasets"]["train"].get("log_low", False):
+            lr_t = torch.log(torch.clamp(lr_t + 1e-3, min=1e-3))
+        if opt.get("concat_histeq", False):
+            his = t(his)
+            lr_t = torch.cat([lr_t, his], dim=1)
+        heat = opt['heat']
+        with torch.cuda.amp.autocast():
+            # sr_t = model.get_sr(lq=lr_t, heat=None)
+            sr_t = model.get_sr(lq=lr_t.cuda(), heat=None)
+
+        sr = rgb(torch.clamp(sr_t, 0, 1)[:, :, padding_params[0]:sr_t.shape[2] - padding_params[1],
+                 padding_params[2]:sr_t.shape[3] - padding_params[3]])
+        assert raw_shape == sr.shape
+        path_out_sr = os.path.join(out_dir, os.path.basename(lr_path))
+        imwrite(path_out_sr, sr)
