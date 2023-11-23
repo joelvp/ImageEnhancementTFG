252f8c8cc3c5755d85a44557b36c0a4002a66dd8
joelvp <jvaldep@teleco.upv.es>
2023-11-23 15:15:01 +0100

    IMPROVE interface pipeline

--- src/gui.py
+++ src/gui.py
@@ -1,38 +1,75 @@
 import gradio as gr
+import numpy as np
 from PIL import Image
-from models.Deep_White_Balance.PyTorch.white_balance import white_balance_gui
-# from models.LLFlow.code.lowlight import lowlight
+from models.Deep_White_Balance.PyTorch.white_balance import load_wb_model, white_balance_gui
+from models.LLFlow.code.lowlight import load_ll_model, lowlight_gui
+import logging
 
-def image_enhancement(input_images, options):
+logging.basicConfig(level=logging.INFO)
+
+def sepia(input_image):
+    
+    sepia_filter = np.array([
+        [0.393, 0.769, 0.189],
+        [0.349, 0.686, 0.168],
+        [0.272, 0.534, 0.131]
+    ])
+    
+    input_image = np.array(input_image)
+    
+    sepia_img = input_image.dot(sepia_filter.T)
+    sepia_img /= sepia_img.max()
+    
+    return sepia_img
+
+def apply_transformations(input_images, options):
+    
     enhanced_images = []
+    
+    # Flags for load models only one time
+    wb_flag = False
+    ll_flag = False
+    
+    for image in input_images:
+        
+        # TODO: Pensar que es mejor ya que WB usa Pillow y LowLight Numpy
+        image = Image.open(image) # WB uses Pillow, LowLight uses numpy
 
-    for input_image_path in input_images:
-        input_image = Image.open(input_image_path)
-
-        result_image = input_image
         if "White Balance" in options:
-            print("Applying White Balance")
-            white_balance_gui(input_images)
+            if not wb_flag:
+                logging.info('Loading AWB model')
+                net_awb = load_wb_model()
+                wb_flag = True
+                
+            logging.info("Applying White Balance")
+            image = white_balance_gui(image, net_awb)
 
         if "Low Light" in options:
-            print("Applying Low Light")
-            # lowlight()
+            if not ll_flag:
+                logging.info("Loading Low Light model")
+                model, opt = load_ll_model()
+                ll_flag = True
+                
+            logging.info("Applying Low Light")
+            image = lowlight_gui(image, model, opt)
 
         if "Blur" in options:
-            print("Applying Blur")
+            logging.info("Applying Blur")
 
-        enhanced_images.append(result_image)
+        if "Sepia" in options:
+            logging.info("Applying Sepia")
+            image = sepia(image)
+            
+        enhanced_images.append(image)
+
 
     return enhanced_images
 
-def show_original_images(original_images):
+def update_images(input_images): return input_images
 
-    return original_images
 
 with gr.Blocks() as demo:
     input_images = gr.File(type="filepath", label="Input Images", file_count="multiple", file_types=["image"], interactive=True)
-    
-    btn_show = gr.Button("Show Original Images", scale=0)
     
     input_gallery = gr.Gallery(
         label="Input Images",
@@ -44,12 +81,12 @@ with gr.Blocks() as demo:
         height="auto"
     )
     
-    btn_show.click(show_original_images, [input_images] , input_gallery)
+    input_images.change(update_images, [input_images], input_gallery)
 
-    options = gr.CheckboxGroup(["White Balance", "Low Light", "Blur"], label="Options", info="Enhance the image")
+    options = gr.CheckboxGroup(["White Balance", "Low Light", "Blur", "Sepia"], label="Options", info="Enhance the image")
     
     btn_submit = gr.Button("Submit", scale=0)
-
+    
     output_gallery = gr.Gallery(
         label="Output Images",
         elem_id="output_gallery",
@@ -60,7 +97,7 @@ with gr.Blocks() as demo:
         height="auto"
     )
 
-    btn_submit.click(image_enhancement, [input_images, options], output_gallery)
+    btn_submit.click(apply_transformations, [input_images, options], output_gallery)
 
 if __name__ == "__main__":
     demo.launch()
--- src/models/LLFlow/code/lowlight.py
+++ src/models/LLFlow/code/lowlight.py
@@ -1,4 +1,5 @@
 from models.LLFlow.code.test_unpaired import *
+from PIL import Image
 
 def lowlight(conf_path="./models/LLFlow/code/confs/LOLv2-pc.yml",out_dir="../data/demo_images/output/"):
 
@@ -33,3 +34,39 @@ def lowlight(conf_path="./models/LLFlow/code/confs/LOLv2-pc.yml",out_dir="../dat
         assert raw_shape == sr.shape
         path_out_sr = os.path.join(out_dir, os.path.basename(lr_path))
         imwrite(path_out_sr, sr)
+        
+def load_ll_model(conf_path="./models/LLFlow/code/confs/LOLv2-pc.yml"):
+    model, opt = load_model(conf_path)
+    model.netG = model.netG.cuda()
+    
+    return model, opt
+    
+         
+def lowlight_gui(input_image, model, opt):
+    
+    # Convertir la imagen de Pillow a una matriz NumPy
+    image = np.array(input_image)
+    # If the image is in floating-point format, scale it to the range [0, 255]
+    if image.dtype == np.float32 or image.dtype == np.float64:
+        image = (image * 255).astype(np.uint8)
+
+    raw_shape = image.shape
+    image, padding_params = auto_padding(image)
+    his = hiseq_color_cv2_img(image)
+    lr_t = t(image)
+    if opt["datasets"]["train"].get("log_low", False):
+        lr_t = torch.log(torch.clamp(lr_t + 1e-3, min=1e-3))
+    if opt.get("concat_histeq", False):
+        his = t(his)
+        lr_t = torch.cat([lr_t, his], dim=1)
+    with torch.cuda.amp.autocast():
+        sr_t = model.get_sr(lq=lr_t.cuda(), heat=None)
+
+    sr = rgb(torch.clamp(sr_t, 0, 1)[:, :, padding_params[0]:sr_t.shape[2] - padding_params[1],
+                padding_params[2]:sr_t.shape[3] - padding_params[3]])
+    assert raw_shape == sr.shape
+    
+    return Image.fromarray(sr.astype('uint8')) # Return Pillow image
+
+    
+    
