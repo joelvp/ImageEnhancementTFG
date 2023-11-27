e18402df5658af3948787d2a4dd228079a9ffb74
joelvp <jvaldep@teleco.upv.es>
2023-11-27 15:51:35 +0100

    ADD sky image input

--- src/gui.py
+++ src/gui.py
@@ -7,89 +7,96 @@ from models.Deep_White_Balance.PyTorch.white_balance import load_wb_model, white
 from models.LLFlow.code.lowlight import load_ll_model, lowlight_gui
 from models.NAFNet.denoise import load_denoising_model, denoising_gui
 
-
 logging.basicConfig(level=logging.INFO)
 
 
 def imread(img_path):
-  img = cv2.imread(img_path)
-  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  return img
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
 
 def sepia(input_image):
-    
     sepia_filter = np.array([
         [0.393, 0.769, 0.189],
         [0.349, 0.686, 0.168],
         [0.272, 0.534, 0.131]
     ])
-    
     input_image = np.array(input_image)
-    
     sepia_img = input_image.dot(sepia_filter.T)
     sepia_img /= sepia_img.max()
-    
     return sepia_img
 
-def apply_transformations(input_images, options):
-    
+def apply_transformations(input_images, options, sky_image=None):
     enhanced_images = []
-    
+
     # Flags for load models only one time
     wb_flag = False
     ll_flag = False
     denoise_flag = False
-    
+    sky_flag = False
+
     for image in input_images:
-        
         # Numpy array
-        image = imread(image) 
+        image = imread(image)
 
         if "Low Light" in options:
             if not ll_flag:
                 logging.info("Loading Low Light model")
                 model, opt = load_ll_model()
                 ll_flag = True
-                
+
             logging.info("Applying Low Light")
             image = lowlight_gui(image, model, opt)
-        
+
         if "Denoise" in options:
             if not denoise_flag:
                 logging.info("Loading Denoising model")
                 model = load_denoising_model()
-                ll_flag = True
-                
+                denoise_flag = True
+
             logging.info("Applying Denoising")
             image = denoising_gui(image, model)
-            
+
         if "White Balance" in options:
             if not wb_flag:
                 logging.info('Loading AWB model')
                 net_awb = load_wb_model()
                 wb_flag = True
-                
+
             logging.info("Applying White Balance")
             image = white_balance_gui(image, net_awb)
+            
+        if "Sky" in options:
+            logging.info("Applying Sky Replacement")
 
+            if sky_image == None:
+                raise gr.Error("Sky image needed!")
+            
         if "Deblur" in options:
             logging.info("Applying Blur")
 
         if "Sepia" in options:
             logging.info("Applying Sepia")
             image = sepia(image)
-            
+
         enhanced_images.append(image)
+
 
 
     return enhanced_images
 
 def update_images(input_images): return input_images
 
+def switch(options: list):
+    """ Sky image input visibility""" 
+    if "Sky" in options:
+        return gr.Image(visible=True)
+    else:
+        return gr.Image(visible=False)
 
 with gr.Blocks() as demo:
     input_images = gr.File(type="filepath", label="Input Images", file_count="multiple", file_types=["image"], interactive=True)
-    
+
     input_gallery = gr.Gallery(
         label="Input Images",
         elem_id="input_gallery",
@@ -99,13 +106,20 @@ with gr.Blocks() as demo:
         object_fit="contain",
         height="auto"
     )
-    
+
     input_images.change(update_images, [input_images], input_gallery)
 
-    options = gr.CheckboxGroup(["Low Light","Denoise","White Balance", "Blur", "Sepia"], label="Options", info="Enhance the image")
+    options = gr.CheckboxGroup(["Low Light", "Denoise", "White Balance", "Sky", "Blur", "Sepia"],
+                               label="Options", info="Enhance the image")
+
+    # Sky image input (initially hidden)
+    sky_image_input = gr.Image(elem_id="sky_image_input", label="Sky Image", visible=False)
     
+    # Sky image input depends on Sky CheckBox
+    options.change(switch, options, sky_image_input)
+
     btn_submit = gr.Button("Submit", scale=0)
-    
+
     output_gallery = gr.Gallery(
         label="Output Images",
         elem_id="output_gallery",
@@ -116,7 +130,7 @@ with gr.Blocks() as demo:
         height="auto"
     )
 
-    btn_submit.click(apply_transformations, [input_images, options], output_gallery)
+    btn_submit.click(apply_transformations, [input_images, options, sky_image_input], output_gallery)
 
 if __name__ == "__main__":
     demo.launch()
