a0c4eeec72c9227944e502ae1528c6776e873c09
joelvp <jvaldep@teleco.upv.es>
2024-01-07 00:31:53 +0100

    REFACTOR gui.py with aux_functions and ModelManager creation to fix problem of reloading models

--- gui.py
+++ gui.py
@@ -1,79 +1,11 @@
 import gradio as gr
 import numpy as np
 import logging
-import cv2
-import os
+from src.aux_functions import apply_transformations
 
-from models.Deep_White_Balance.PyTorch.white_balance import load_wb_model, white_balance_gui
-from models.LLFlow.code.lowlight import load_ll_model, lowlight_gui
-from models.NAFNet.denoise import load_denoising_model, denoising_gui
-from models.SkyAR.sky_replace import load_sky_model, sky_replace_gui
+from src.model_manager import ModelManager
 
 logging.basicConfig(level=logging.INFO)
-
-
-def imread(img_path):
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    return img
-
-def apply_transformations(input_images, options, sky_image=None):
-
-    enhanced_images = []
-
-    # Flags for load models only one time
-    wb_flag = False
-    ll_flag = False
-    denoise_flag = False
-    sky_flag = False
-
-    for image in input_images:
-        # Numpy array
-        image = imread(image)
-
-        for option in options:
-            if option == "Low Light":
-                if not ll_flag:
-                    logging.info("Loading Low Light model")
-                    model, opt = load_ll_model()
-                    ll_flag = True
-
-                logging.info("Applying Low Light")
-                image = lowlight_gui(image, model, opt)
-
-            elif option == "Denoise":
-                if not denoise_flag:
-                    logging.info("Loading Denoising model")
-                    model = load_denoising_model()
-                    denoise_flag = True
-
-                logging.info("Applying Denoising")
-                image = denoising_gui(image, model)
-
-            elif option == "White Balance":
-                if not wb_flag:
-                    logging.info('Loading AWB model')
-                    net_awb = load_wb_model()
-                    wb_flag = True
-
-                logging.info("Applying White Balance")
-                image = white_balance_gui(image, net_awb)
-                
-            elif option == "Sky":
-                if sky_image is None:
-                    raise gr.Error("Sky image needed!")
-                
-                if not sky_flag:
-                    logging.info('Loading Sky Replacement model')
-                    model, config = load_sky_model()
-                    sky_flag = True
-                    
-                logging.info("Applying Sky Replacement")
-                image = sky_replace_gui(image, sky_image, model, config)
-
-        enhanced_images.append(image)
-
-    return enhanced_images
 
 def update_images(input_images): return input_images
 
@@ -85,60 +17,62 @@ def switch(options: list):
     else:
         return gr.Image(visible=False)
 
-with gr.Blocks() as demo:
-    input_images = gr.File(type="filepath", label="Input Images", file_count="multiple", file_types=["image"], interactive=True)
+def apply_transformations_event(input_images, options, sky_image_input):
+    # Funcion para poder usar model_manager en el contexto Gradio, es una funcion auxiliar
+    # Gradio ejecuta una funcion con todo argumentos de tipo Gradio y dentro de esta funcion ya usamos la funcion original y instancia de ModelManager
 
-    input_gallery = gr.Gallery(
-        label="Input Images",
-        elem_id="input_gallery",
-        show_label=False,
-        columns=[3],
-        rows=[1],
-        object_fit="contain",
-        height="auto"
-    )
-
-    input_images.change(update_images, [input_images], input_gallery)
-
-    options = gr.Dropdown(
-            ["Low Light", "Denoise", "White Balance", "Sky"], value=["White Balance"], multiselect=True,
-            label="Choose the filters according to the order in which you want to apply them. "
-        )
-
-    # Sky image input (initially hidden)
-    sky_image_input = gr.Image(
-    "data/demo_images/skybox/galaxy.jpg",
-    elem_id="sky_image_input",
-    label="Sky Image",
-    visible=False,
-    height=500,  # Establece la altura máxima de la imagen
-    width=10000    # Establece la anchura máxima de la imagen
-    )
-    
-    # Sky image input depends on Sky CheckBox
-    options.change(switch, options, sky_image_input)
-
-    btn_submit = gr.Button("Submit", scale=0)
-
-    output_gallery = gr.Gallery(
-        label="Output Images",
-        elem_id="output_gallery",
-        show_label=False,
-        columns=[3],
-        rows=[1],
-        object_fit="contain",
-        height="auto"
-    )
-
-    btn_submit.click(apply_transformations, [input_images, options, sky_image_input], output_gallery)
+    enhanced_images = apply_transformations(input_images, options, model_manager, sky_image_input)
+    return enhanced_images
 
 if __name__ == "__main__":
+    
+    model_manager = ModelManager()
+    
+    with gr.Blocks() as demo:
+        input_images = gr.File(type="filepath", label="Input Images", file_count="multiple", file_types=["image"], interactive=True)
+
+        input_gallery = gr.Gallery(
+            label="Input Images",
+            elem_id="input_gallery",
+            show_label=False,
+            columns=[3],
+            rows=[1],
+            object_fit="contain",
+            height="auto"
+        )
+
+        input_images.change(update_images, [input_images], input_gallery)
+
+        options = gr.Dropdown(
+                ["Low Light", "Denoise", "White Balance", "Sky"], value=["White Balance"], multiselect=True,
+                label="Choose the filters according to the order in which you want to apply them. "
+            )
+
+        # Sky image input (initially hidden)
+        sky_image_input = gr.Image(
+        "data/demo_images/skybox/galaxy.jpg",
+        elem_id="sky_image_input",
+        label="Sky Image",
+        visible=False,
+        height=500,  # Establece la altura máxima de la imagen
+        width=10000    # Establece la anchura máxima de la imagen
+        )
+        
+        # Sky image input depends on Sky CheckBox
+        options.change(switch, options, sky_image_input)
+
+        btn_submit = gr.Button("Submit", scale=0)
+
+        output_gallery = gr.Gallery(
+            label="Output Images",
+            elem_id="output_gallery",
+            show_label=False,
+            columns=[3],
+            rows=[1],
+            object_fit="contain",
+            height="auto"
+        )
+
+        btn_submit.click(apply_transformations_event, [input_images, options, sky_image_input], output_gallery)
+        
     demo.launch()
-    
-    
-##### HOW TO RUN #####
-    """
-    cd src
-    python gui.py
-    
-    """
---
+++ src/aux_functions.py
@@ -0,0 +1,63 @@
+from typing import List
+import cv2
+import logging
+import gradio as gr
+import numpy as np
+
+from .model_manager import ModelManager
+
+from models.Deep_White_Balance.PyTorch.white_balance import white_balance_gui
+from models.LLFlow.code.lowlight import  lowlight_gui
+from models.NAFNet.denoise import  denoising_gui
+from models.SkyAR.sky_replace import  sky_replace_gui
+
+
+def imread(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+def apply_transformations(input_images, options, model_manager:ModelManager, sky_image=None) -> List[np.ndarray]:
+    enhanced_images = []
+
+    for image in input_images:
+        # Numpy array
+        image = imread(image)
+
+        for option in options:
+            if option == "Low Light":
+                if model_manager.ll_model is None:
+                    model_manager.load_ll_model()
+
+                logging.info("Applying Low Light")
+                image = lowlight_gui(image, model_manager.ll_model, model_manager.opt_ll)
+
+            elif option == "Denoise":
+                if model_manager.denoise_model is None:
+                    model_manager.load_denoise_model()
+
+                logging.info("Applying Denoising")
+                image = denoising_gui(image, model_manager.denoise_model)
+
+            elif option == "White Balance":
+                if model_manager.wb_model is None:
+                    model_manager.load_wb_model()
+
+                logging.info("Applying White Balance")
+                image = white_balance_gui(image, model_manager.wb_model)
+
+            elif option == "Sky":
+                if sky_image is None:
+                    raise gr.Error("Sky image needed!")
+
+                if model_manager.sky_model is None:
+                    model_manager.load_sky_model()
+
+                logging.info("Applying Sky Replacement")
+                image = sky_replace_gui(image, sky_image, model_manager.sky_model, model_manager.sky_config)
+
+        enhanced_images.append(image)
+        
+    logging.info("Filters correctly apllied!")
+
+    return enhanced_images
